58fd744bdebe67df47e78104e69dee6d9bd4e723
cwh <cwh02057642@alibaba-inc.com>
2026-06-10 09:55:52 +0800

    feat(frontend): ✨  loginPage 改造

--- apps/frontend/monitor/src/components/login-form.tsx
+++ apps/frontend/monitor/src/components/login-form.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
-import { Field, FieldDescription, FieldGroup, FieldLabel, FieldSeparator } from '@/components/ui/field';
+import { Field, FieldDescription, FieldGroup, FieldLabel } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 
@@ -8,16 +8,16 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
     return (
         <div className={cn('flex flex-col gap-6', className)} {...props}>
             <Card className="overflow-hidden p-0">
-                <CardContent className="grid p-0 md:grid-cols-2">
+                <CardContent className="grid p-0">
                     <form className="p-6 md:p-8">
                         <FieldGroup>
                             <div className="flex flex-col items-center gap-2 text-center">
                                 <h1 className="text-2xl font-bold">Welcome back</h1>
-                                <p className="text-balance text-muted-foreground">Login to your Acme Inc account</p>
+                                <p className="text-balance text-muted-foreground">Login to your monitor account</p>
                             </div>
                             <Field>
-                                <FieldLabel htmlFor="email">Email</FieldLabel>
-                                <Input id="email" type="email" placeholder="m@example.com" required />
+                                <FieldLabel htmlFor="username">Username</FieldLabel>
+                                <Input id="username" type="username" placeholder="Username" required />
                             </Field>
                             <Field>
                                 <div className="flex items-center">
@@ -31,7 +31,7 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
                             <Field>
                                 <Button type="submit">Login</Button>
                             </Field>
-                            <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">Or continue with</FieldSeparator>
+                            {/* <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">Or continue with</FieldSeparator>
                             <Field className="grid grid-cols-3 gap-4">
                                 <Button variant="outline" type="button">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -60,19 +60,19 @@ export function LoginForm({ className, ...props }: React.ComponentProps<'div'>) 
                                     </svg>
                                     <span className="sr-only">Login with Meta</span>
                                 </Button>
-                            </Field>
+                            </Field> */}
                             <FieldDescription className="text-center">
                                 Don&apos;t have an account? <a href="#">Sign up</a>
                             </FieldDescription>
                         </FieldGroup>
                     </form>
-                    <div className="relative hidden bg-muted md:block">
+                    {/* <div className="relative hidden bg-muted md:block">
                         <img
                             src="/placeholder.svg"
                             alt="Image"
                             className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
                         />
-                    </div>
+                    </div> */}
                 </CardContent>
             </Card>
             <FieldDescription className="px-6 text-center">
